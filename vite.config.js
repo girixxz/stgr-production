ecbbec4ea6e385802be29d9b508cb6a258db236d
girixxz <adityagiri206@gmail.com>
2025-10-27 20:12:31 +0700

    Fix vite.config.js - Add manifest and build configuration for production

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,13 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        manifest: true,
+        outDir: "public/build",
+        rollupOptions: {
+            output: {
+                manualChunks: undefined,
+            },
+        },
+    },
 });
