5c796b9b42197235c4ec3e0d087cd35afc2a2619
girixxz <adityagiri206@gmail.com>
2025-10-27 19:56:45 +0700

    Fix UserSeeder: Remove Faker dependency for production compatibility

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,32 +13,55 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        $password = Hash::make('password123');
+        
         // Create one user for each role
-        User::factory()->create([
+        User::create([
             'fullname' => 'Owner User',
             'username' => 'owner',
+            'phone_number' => '081234567890',
+            'password' => $password,
             'role' => 'owner',
         ]);
 
-        User::factory()->create([
+        User::create([
             'fullname' => 'Admin User',
             'username' => 'admin',
+            'phone_number' => '081234567891',
+            'password' => $password,
             'role' => 'admin',
         ]);
 
-        User::factory()->create([
-            'fullname' => 'PM User',
-            'username' => 'pm',
+        User::create([
+            'fullname' => 'Project Manager 1',
+            'username' => 'pm1',
+            'phone_number' => '081234567892',
+            'password' => $password,
             'role' => 'pm',
         ]);
 
-        User::factory()->create([
-            'fullname' => 'Karyawan User',
-            'username' => 'karyawan',
+        User::create([
+            'fullname' => 'Project Manager 2',
+            'username' => 'pm2',
+            'phone_number' => '081234567893',
+            'password' => $password,
+            'role' => 'pm',
+        ]);
+
+        User::create([
+            'fullname' => 'Karyawan Produksi 1',
+            'username' => 'karyawan1',
+            'phone_number' => '081234567894',
+            'password' => $password,
             'role' => 'karyawan',
         ]);
 
-        // Create additional random users
-        User::factory(10)->create();
+        User::create([
+            'fullname' => 'Karyawan Produksi 2',
+            'username' => 'karyawan2',
+            'phone_number' => '081234567895',
+            'password' => $password,
+            'role' => 'karyawan',
+        ]);
     }
 }
